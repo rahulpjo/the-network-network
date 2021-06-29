23662859ea3f03b4d8cef4fc98311cf7a663f94f
rahulpjo <rjoshi9000@gmail.com>
2021-06-29 13:45:14 -0700

    Add form and more styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,23 @@
 function Form(props) {
-  console.log(props.note);
-  return <article></article>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <article className="selected-post">
+      <section className="post">
+        <h3>Create a New Post</h3>
+        <form>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+          <label htmlFor="text">Post Body</label>
+          <input type="text" id="text" />
+          <button type="submit">
+            <h3>Submit</h3>
+          </button>
+        </form>
+      </section>
+    </article>
+  );
 }
 
 export default Form;
